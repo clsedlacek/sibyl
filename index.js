c9bbe680eafa0b1a1ef3539c5d3c8f40fa60304e
Chris <christian.l.sedlacek@gmail.com>
2018-05-13 17:22:19 -0500

    Remove redundant message event

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.commands.set('meetup', require('./commands/meetup.js'));
 //setting all the commands and locations
 
 client.on('message', message => require("./events/message.js")(client, message));
-//call handleMessage(message) on client.on('message') event; event call provides function parameter ('message') automagically
+//call handleMessage(message) on client.on('message') event; // event call provides function parameter ('message') automagically
 client.on('guildCreate', guild => require('./events/guildCreate.js')(client, guild));
 client.on('ready', () => require('./events/ready.js')(client));
 client.on('guildMemberAdd', member => require('./events/guildMemberAdd.js')(client, member));
