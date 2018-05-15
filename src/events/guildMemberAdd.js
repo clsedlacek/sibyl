12142cb8b17e73f67a7c7aa83649f0858d52cd60
Chris <christian.l.sedlacek@gmail.com>
2018-05-14 23:31:51 -0500

    Added missing closing bracket

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,7 +1,10 @@
+const channels = require('../../config/channels.json');
+
 module.exports = (client, member) => {
     console.log('Log', `${member.user.tag} (${member.id}) has joined ${member.guild.name} (${member.guild.id})`);
     //log when a user joins server
 
-    member.guild.channels.find("name", "introductions").send("**Welcome to the server, <@" + member.id + ">!** In order to access the chat channels, write your A/S/L and something about yourself here. Please take some time to read the rules and guidelines in #server and enjoy your stay!");
+    member.guild.channels.find("id", channels.introductions).send("**Welcome to the server, <@" + member.id + ">!** In order to access the chat channels, write your A/S/L and something about yourself here. Please take some time to read the rules and guidelines in #server and enjoy your stay!");
     
 //send welcome message if welcome channel exists
+}
