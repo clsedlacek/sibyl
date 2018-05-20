11dbc16f0f883b55d2e46353fef811f806e80e32
Chris <christian.l.sedlacek@gmail.com>
2018-05-20 02:11:27 -0500

    Refactor dice into new module

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -1,21 +1,14 @@
 const Discord = require('discord.js');
-const d20 = require('d20');
+const dice = require('../features/dice.js');
 
 module.exports = (client, message, args) => {
 	console.log('Log', 'rolled...');
 
 	if (args.length === 0) {
-		message.channel.send(`Please specify a die type in one of the following formats: d*x*, *y*d*x*, *y*d*x*+/-*z*, or simply *x*.`)
+		dice.sendFailMessage(message.channel);
 		return
 	};
 
-	var dice = args.toString();
-	result = d20.roll(dice);
-
-	// message.channel.send("You roll " + dice + " and get " + result); // old message response, replaced with embed
-	const embed = new Discord.RichEmbed()       // this will need to be updated to Discord.MessageEmbed once the Discord.js version is updated to version 12
-	.setDescription(message.author.username + ' rolls ' + dice + '!')
-	.setThumbnail("http://mtgs.me/s/discordbots/dicespill.jpg")
-	.addField('Result:', result)
-	message.channel.send({ embed });       // embed variable is the same as the object so this could just be message.channel.send({ embed });
+	let diceRoll = dice.rollDice(args.toString(), message.member);
+	dice.sendDiceEmbed(diceRoll, message.channel);
 };
