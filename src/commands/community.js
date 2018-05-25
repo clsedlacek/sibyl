d19d4f0b620a101d3834a847529f0683806d605c
Chris <christian.l.sedlacek@gmail.com>
2018-05-25 15:33:24 -0500

    Add community command actions and incorrect permissions messages

--- src/commands/community.js
+++ src/commands/community.js
@@ -3,9 +3,16 @@ const community = require('../features/moderation/community.js');
 
 module.exports = (client, message, args) => {
 	console.log('Log', 'add-to-community command call');
-	let callingUser = message.member;
+	let callingMember = message.member;
 
 	if (args.length === 0) {
 		return community.sendAddCommunityFailMessage(message.channel);
 	}; 
+
+	if (community.checkAddCommunityPermissions(callingMember)) {
+		community.addMemberCommunity(callingMember, args[0]);
+	}
+	else {
+		community.sendIncorrectPermissionsMessage(message.channel);
+	}
 }
--- src/features/moderation/community.js
+++ src/features/moderation/community.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
+const moderationConfig = require('../../../config/moderation.json');
 const roles = require('../../../config/roles.json');
+const channels = require('../../../config/channels.json');
 
 module.exports = {
 	checkAddCommunityPermissions: function(guildMember) {
@@ -10,7 +12,22 @@ module.exports = {
 		return false;
 	},
 
+	addMemberCommunity: function(callingMember, userTagString) {
+		const addingMember = callingMember.guild.members.find(member => member.user.tag === userTagString);
+		const communityRole = callingMember.guild.roles.find('name', roles.community);
+		const communityNotificationMessage = (`Welcome <@${addingMember.user.id}> -- you have been elected to the @Community role and now have access to <#${channels.braintrust}> and other protected channels. As outlined in <#${channels.server}>, @Community members can help peer-moderate by responding to offensive messages with a ${moderationConfig.muteReact} react. If you would like still more server access, you can be @Certified by declaring your real-life location to join the <#${channels.meetups}> channel by entering !meetup <your location>, or gain the @Librarian role by submitting to <#${channels.library}>.`);
+
+		return addingMember.addRole(communityRole)
+		.then(() => {
+			return callingMember.guild.channels.find("id", channels.braintrust).send(communityNotificationMessage);
+		}).catch(console.error);
+	},
+
 	sendAddCommunityFailMessage: function(guildChannel) {
 		return guildChannel.send(`Please specify which member to add to ${roles.community}. Example: \`!community [member]\``)
 	},
+
+	sendIncorrectPermissionsMessage: function(guildChannel) {
+		return guildChannel.send(`Command only available to ${roles.operator} role`)            
+	}
 }
