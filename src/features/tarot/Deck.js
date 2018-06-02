31707c95979c3fbf85fb6061e2b2a266a7f89ef1
Chris <christian.l.sedlacek@gmail.com>
2018-06-01 21:22:16 -0500

    Add shuffle and draw features to deck

--- src/features/tarot/Deck.js
+++ src/features/tarot/Deck.js
@@ -14,11 +14,47 @@ class TarotDeck {
 		this.cardsBase = createCardData(deckData.cards); // reference for all total cards in deck
 		this.cardsRemaining = [...this.cardsBase]; // shows cards currently "in the deck" for if someone is using it
 	}
+	shuffle() {
+		const cardsRemaining = [...this.cardsBase];
+
+		// fisher-yates shuffle
+		let currentIndex = cardsRemaining.length, temporaryValue, randomIndex;
+		while (currentIndex !== 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			temporaryValue = cardsRemaining[currentIndex];
+			cardsRemaining[currentIndex] = cardsRemaining[randomIndex];
+			cardsRemaining[randomIndex] = temporaryValue;
+		}
+		this.cardsRemaining = cardsRemaining;
+
+		return this.cardsRemaining;
+	}
+	drawRandomCard() {
+		const randomIndex = Math.floor(Math.random() * this.cardsRemaining.length);
+		return this.cardsRemaining.splice(randomIndex, 1);
+	}
+	drawSpecifiedCard(cardName, cardNumber) {
+		for (let i=0; i<this.cardsRemaining.length; i++) {
+			let cardChecking = this.cardsRemaining[i];
+			if ((cardName.toLowerCase() === cardChecking.name.toLowerCase()) && (cardNumber === cardChecking.number)) {
+				console.log('card found');
+				return this.cardsRemaining.splice(i, 1);
+			}
+		}
+		console.log('card not found');
+		return null;
+	}
 }
 
 module.exports = {
 	TarotDeck,
 	createTarotDeck: function(deckData) {
-		return new TarotDeck(deckData);
+		const deck = new TarotDeck(deckData);
+		deck.shuffle();
+		console.log('cardsRemaining:');
+		console.dir(deck.cardsRemaining);
+		return deck;
 	}
 }
